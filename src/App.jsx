08bcc4e653a611ab761c21cc5517a73a303ac30a
jwseph <kodomo.oishii@gmail.com>
2023-08-06 19:53:06 -0700

    Changed tictactoe board, other GUI updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import { GoGlobe, GoPeople, GoSmiley }from 'react-icons/go'
 import { Route, Link, Routes, useNavigate, useParams } from 'react-router-dom'
@@ -7,90 +7,118 @@ const classNames = (...classes) => classes.filter(Boolean).join(' ');
 
 const PLAYERS = 'OX';
 
-function Mark({type, hidden}) {
+function O() {
+  return (
+    <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+      <circle cx='50' cy='50' r='46' fill='none' stroke='currentColor' strokeWidth='8'/>
+    </svg>
+  )
+}
+
+function X() {
+  return (
+    <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+      <line x1='8' y1='8' x2='92' y2='92' stroke='currentColor' strokeWidth='8'/>
+      <line x1='92' y1='8' x2='8' y2='92' stroke='currentColor' strokeWidth='8'/>
+    </svg>
+  )
+}
+
+function Mark({type, hidden, transparent}) {
+  useEffect(() => {
+    console.log('mark', type, hidden, transparent);
+  }, [])
+  useEffect(() => {
+    console.log('transparency changed')
+  }, [transparent])
   return type == 0 ? (
     <div className={classNames(
-      'w-full h-full text-center p-6 duration-300 ease-in-out',
-      hidden ? 'opacity-0 hover:opacity-30 hover:cursor-pointer' : 'opacity-90',
+      'w-full h-full text-center p-5',
+      transparent ? 'become-transparent' : hidden ? 'text-black hover:text-zinc-900 cursor-pointer' : 'place opacity-100',
     )}>
-      <div className='w-full h-full rounded-full p-[4px] bg-gradient-to-br from-zinc-50 via-zinc-100 to-zinc-200'>
-        <div className='w-full h-full rounded-full bg-black'></div>
-      </div>
+      <O/>
     </div>
   ) : type == 1 ? (
     <div className={classNames(
-      'w-full h-full flex justify-center items-center p-6 duration-300 ease-in-out',
-      hidden ? 'opacity-0 hover:opacity-30 hover:cursor-pointer' : 'opacity-90',
+      'w-full h-full flex justify-center items-center p-5',
+      transparent ? 'become-transparent' : hidden ? 'text-black hover:text-zinc-900 cursor-pointer' : 'place opacity-100',
     )}>
-      <div className='w-[4px] h-[calc(125%-4px)] rotate-45 relative bg-zinc-100'></div>
-      <div className='w-[4px] h-[calc(125%-4px)] rotate-[135deg] relative -ml-[4px] bg-gradient-to-t from-zinc-50 via-zinc-100 to-zinc-200'></div>
+      <X/>
     </div>
   ) : (
     <div className='w-full h-full'></div>
   );
 }
 
-function Board({gameState, player, onClick, disabled}) {
+function Board({gameState, winningState, player, onClick, disabled}) {
   return (
     <div className='fade-in'>
-      <div className='w-full aspect-square flex flex-col border-2 border-zinc-700 rounded-xl'>
+      <div className='w-full aspect-square flex flex-col'>
         <div className='flex-1 w-full flex'>
-          <div className='flex-1 h-full border border-zinc-700 border-t-0 border-l-0'>
+          <div className='flex-1 h-full'>
             {gameState>>9&1
-              ? <Mark type={gameState&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(0)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState&1} transparent={winningState && !(winningState&1)}/>
+              : !disabled && !winningState && <div className='w-full h-full cursor-pointer' onClick={() => onClick(0)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700 border-t-0'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+1&1
-              ? <Mark type={gameState>>1&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(1)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>1&1} transparent={winningState && !(winningState>>1&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(1)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700 border-t-0 border-r-0'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+2&1
-              ? <Mark type={gameState>>2&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(2)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>2&1} transparent={winningState && !(winningState>>2&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(2)}><Mark type={player} hidden/></div>
             }
           </div>
         </div>
+        <div className='h-[5px] bg-zinc-700'></div>
         <div className='flex-1 w-full flex'>
-          <div className='flex-1 h-full border border-zinc-700 border-l-0'>
+          <div className='flex-1 h-full'>
             {gameState>>9+3&1
-              ? <Mark type={gameState>>3&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(3)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>3&1} transparent={winningState && !(winningState>>3&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(3)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+4&1
-              ? <Mark type={gameState>>4&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(4)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>4&1} transparent={winningState && !(winningState>>4&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(4)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700 border-r-0'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+5&1
-              ? <Mark type={gameState>>5&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(5)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>5&1} transparent={winningState && !(winningState>>5&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(5)}><Mark type={player} hidden/></div>
             }
           </div>
         </div>
+        <div className='h-[5px] bg-zinc-700'></div>
         <div className='flex-1 w-full flex'>
-          <div className='flex-1 h-full border border-zinc-700 border-b-0 border-l-0'>
+          <div className='flex-1 h-full'>
             {gameState>>9+6&1
-              ? <Mark type={gameState>>6&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(6)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>6&1} transparent={winningState && !(winningState>>6&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(6)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700 border-b-0'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+7&1
-              ? <Mark type={gameState>>7&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(7)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>7&1} transparent={winningState && !(winningState>>7&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(7)}><Mark type={player} hidden/></div>
             }
           </div>
-          <div className='flex-1 h-full border border-zinc-700 border-b-0 border-r-0'>
+          <div className='w-[5px] bg-zinc-700'></div>
+          <div className='flex-1 h-full'>
             {gameState>>9+8&1
-              ? <Mark type={gameState>>8&1}/>
-              : !disabled && <button className='w-full h-full' onClick={() => onClick(8)}><Mark type={player} hidden/></button>
+              ? <Mark type={gameState>>8&1} transparent={winningState && !(winningState>>8&1)}/>
+              : !disabled && !winningState  && <div className='w-full h-full cursor-pointer' onClick={() => onClick(8)}><Mark type={player} hidden/></div>
             }
           </div>
         </div>
@@ -103,40 +131,26 @@ function SelectModePage() {
   return (
     <>
       {/* <h1 className='text-2xl font-semibold tracking-tighter'>Select gamemode</h1> */}
-      <svg width='0' height='0'>
-        <linearGradient id='blue-gradient' x1='100%' y1='100%' x2='0%' y2='0%'>
-          <stop stopColor='#2563eb' offset='0%'/>
-          <stop stopColor='#0ea5e9' offset='100%'/>
-        </linearGradient>
-        <linearGradient id='yellow-gradient' x1='100%' y1='100%' x2='0%' y2='0%'>
-          <stop stopColor='#f97316' offset='0%'/>
-          <stop stopColor='#eab308' offset='100%'/>
-        </linearGradient>
-        <linearGradient id='purple-gradient' x1='100%' y1='100%' x2='0%' y2='0%'>
-          <stop stopColor='#8b5cf6' offset='0%'/>
-          <stop stopColor='#d946ef' offset='100%'/>
-        </linearGradient>
-      </svg>
       <div className='fade-in flex flex-col gap-2'>
-        <Link to='online' className='group w-full px-5 py-4 sm:rounded-lg flex items-center opacity-50 hover:opacity-100 hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
-          <GoGlobe className='w-10 h-10 text-zinc-500'/>
+        <Link to='/online' className='group w-full px-5 py-4 sm:rounded-lg flex items-center hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
+          <GoGlobe className='w-10 h-10 text-zinc-700 group-hover:text-zinc-500 duration-300 ease-in-out'/>
           <div className='flex-1 font-medium'>
-            <h3 className='text-left text-zinc-200 text-lg'>Online</h3>
-            <p className='text-left text-zinc-500 text-sm'>Play against an online opponent</p>
+            <h3 className='text-left text-zinc-500 group-hover:text-zinc-200 text-lg duration-300 ease-in-out'>Online</h3>
+            <p className='text-left text-zinc-700 group-hover:text-zinc-500 text-sm duration-300 ease-in-out'>Play against an online opponent</p>
           </div>
         </Link>
-        <Link to='/local' className='group w-full px-5 py-4 sm:rounded-lg flex items-center opacity-50 hover:opacity-100 hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
-          <GoPeople className='w-10 h-10 text-zinc-500'/>
+        <Link to='/local' className='group w-full px-5 py-4 sm:rounded-lg flex items-center hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
+          <GoPeople className='w-10 h-10 text-zinc-700 group-hover:text-zinc-500 duration-300 ease-in-out'/>
           <div className='flex-1 font-medium'>
-            <h3 className='text-left text-zinc-200 text-lg'>Local</h3>
-            <p className='text-left text-zinc-500 text-sm'>Play against a friend on this device</p>
+            <h3 className='text-left text-zinc-500 group-hover:text-zinc-200 text-lg duration-300 ease-in-out'>Local</h3>
+            <p className='text-left text-zinc-700 group-hover:text-zinc-500 text-sm duration-300 ease-in-out'>Play against a friend on this device</p>
           </div>
         </Link>
-        <Link to='/computer' className='group w-full px-5 py-4 sm:rounded-lg flex items-center opacity-50 hover:opacity-100 hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
-          <GoSmiley className='w-10 h-10 text-zinc-500'/>
+        <Link to='/computer' className='group w-full px-5 py-4 sm:rounded-lg flex items-center hover:bg-zinc-900/75 duration-300 ease-in-out gap-5'>
+          <GoSmiley className='w-10 h-10 text-zinc-700 group-hover:text-zinc-500 duration-300 ease-in-out'/>
           <div className='flex-1 font-medium'>
-            <h3 className='text-left text-zinc-200 text-lg'>Computer</h3>
-            <p className='text-left text-zinc-500 text-sm'>Play against the computer</p>
+            <h3 className='text-left text-zinc-500 group-hover:text-zinc-200 text-lg duration-300 ease-in-out'>Computer</h3>
+            <p className='text-left text-zinc-700 group-hover:text-zinc-500 text-sm duration-300 ease-in-out'>Play against the computer</p>
           </div>
         </Link>
       </div>
@@ -163,45 +177,79 @@ function LocalPlayPage() {
   const [wins, setWins] = useState([0, 0]);
   const [gameState, setGameState] = useState(0);  // 0b_markedTiles_tileTypes
   const [player, setPlayer] = useState(0);
-  function checkWin(state) {
+  const [winningState, setWinningState] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  function getWinState(state) {
+    for (const winState of WIN_STATES) {
+      if ((state>>9&winState) != winState) continue;
+      let taken = state&0b111111111&winState;
+      if (taken == 0 || taken == winState) return winState;
+    }
+    return 0;
+  }
+  function getWinner(state) {
     for (const winState of WIN_STATES) {
       if ((state>>9&winState) != winState) continue;
       let taken = state&0b111111111&winState;
       if (taken == 0) {
-        console.log('O won')
+        setWinningState(winState);
+        return 0;
       }
       if (taken == winState) {
-        console.log('X won')
+        setWinningState(winState);
+        return 1;
       }
     }
+    return -1;
   }
   return (
     <>
-      <div className='flex'>
-        <div className='fade-in delay-2 px-8 flex gap-2 items-center'>
-          <div className='w-5 h-5 ring-2 ring-inset ring-zinc-100 rounded-full'></div>
+      <div className={classNames('flex duration-300 ease-in-out', player != 0 && 'text-zinc-700')}>
+        <div className='fade-in delay-4 px-8 flex gap-2 items-center'>
+          <div className='w-5 h-5'><O/></div>
           <div className='text-lg font-medium'>{wins[0]}</div>
         </div>
       </div>
-      <div className='w-full px-8'>
-        <Board
-          gameState={gameState}
-          player={player}
-          onClick={(i) => {
-            console.log(i, 'Clicked!');
-            let newGameState = gameState | player<<i | 1<<i+9;
-            setGameState(newGameState);
-            checkWin(newGameState);
-            setPlayer(1-player);
-          }}
-        />
+      <div className='px-8'>
+        <div className={classNames('w-full duration-1000 ease-in-out', gameOver && 'opacity-50')}>
+          <Board
+            gameState={gameState}
+            winningState={winningState}
+            player={player}
+            onClick={(i) => {
+              setPlayer(1-player);
+              console.log(i, 'Clicked!');
+              let newGameState = gameState | player<<i | 1<<i+9;
+              setGameState(newGameState);
+              let newWinState = getWinState(newGameState);
+              console.log(newGameState, newWinState);
+              if (!newWinState && (newGameState&(1<<9)-1<<9) == ((1<<9)-1<<9)) {
+                console.log('Draw');
+                newWinState = 1<<18;
+              }
+              console.log(newWinState);
+              setWinningState(newWinState);
+              if (newWinState) setPlayer(player);
+              return;
+              let winner = getWinner(newGameState);
+              if (winner < 0) return;
+              let newWins = [...wins];
+              newWins[winner]++;
+              setWins(newWins);
+              setGameOver(true);
+              // setGameState(0);
+            }}
+          />
+        </div>
+        {/* <div className='relative w-full aspect-square mt-[-100%] bg-zinc-700/50'>
+          <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+            <circle cx='50' cy='50' r='48' fill='none' stroke='white' strokeWidth='4'/>
+          </svg>
+        </div> */}
       </div>
-      <div className='flex flex-row-reverse'>
-        <div className='fade-in delay-2 px-8 flex flex-row-reverse gap-2 items-center'>
-          <div className='w-5 h-5 flex justify-center items-center'>
-            <div className='w-[2px] h-[calc(125%-2px)] rotate-45 relative bg-zinc-100'></div>
-            <div className='w-[2px] h-[calc(125%-2px)] rotate-[135deg] relative -ml-[2px] bg-gradient-to-t from-zinc-50 via-zinc-100 to-zinc-200'></div>
-          </div>
+      <div className={classNames('flex flex-row-reverse duration-300 ease-in-out', player != 1 && 'text-zinc-700')}>
+        <div className='fade-in delay-4 px-8 flex flex-row-reverse gap-2 items-center'>
+          <div className='w-5 h-5'><X/></div>
           <div className='text-lg font-medium'>{wins[1]}</div>
         </div>
       </div>
@@ -217,6 +265,35 @@ function App() {
           <Route path='/' element={<SelectModePage/>}/>
           <Route path='/local' element={<LocalPlayPage/>}/>
         </Routes>
+      </div>
+      <div className='w-full p-6 flex justify-center bottom-0 absolute sm:bottom-auto sm:top-0 sm:justify-start bg-black/50 backdrop-blur-sm'>
+        <Link to='/' className='fade-in delay-4 p-2 rounded-md text-zinc-700 hover:text-zinc-200 duration-300 ease-in-out'>
+          <div className='w-8 aspect-square flex flex-col'>
+            <div className='flex-1 w-full flex'>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+            </div>
+            <div className='h-[2px] bg-current'></div>
+            <div className='flex-1 w-full flex'>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+            </div>
+            <div className='h-[2px] bg-current'></div>
+            <div className='flex-1 w-full flex'>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+              <div className='w-[2px] bg-current'></div>
+              <div className='flex-1 h-full'></div>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   )
